20ddc2ae6a40c500fc8d8b0bb77bde65981f16e9
Kang Minsu <skymin0417@gmail.com>
2023-03-13 17:41:59 +0900

    fix: 의존성 주입이 누락된 항목들 수정

--- src/route/book/book.module.ts
+++ src/route/book/book.module.ts
@@ -3,10 +3,18 @@ import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { MusicBookRepository } from 'src/common/repository/musicbook/musicbook.repository';
 import { JwtAuthService } from 'src/common/jwt-auth/jwt-auth.service';
+import { MusicBookLikeRepository } from 'src/common/repository/musicbook/musicbook-like.repository';
+import { CloudflareImagesService } from 'src/common/cloudflare/cloudflare-images.service';
 
 @Global()
 @Module({
   controllers: [BookController],
-  providers: [BookService, MusicBookRepository, JwtAuthService],
+  providers: [
+    BookService,
+    MusicBookRepository,
+    MusicBookLikeRepository,
+    CloudflareImagesService,
+    JwtAuthService,
+  ],
 })
 export class BookModule {}
--- src/route/music/music.module.ts
+++ src/route/music/music.module.ts
@@ -4,10 +4,21 @@ import { MusicService } from './music.service';
 import { JwtAuthService } from 'src/common/jwt-auth/jwt-auth.service';
 import { MusicBookRepository } from 'src/common/repository/musicbook/musicbook.repository';
 import { MelonService } from '../melon/melon.service';
+import { MusicBookLikeRepository } from 'src/common/repository/musicbook/musicbook-like.repository';
+import { MusicBookSourceRepository } from 'src/common/repository/musicbook/musicbook-source.repository';
+import { CloudflareImagesService } from 'src/common/cloudflare/cloudflare-images.service';
 
 @Global()
 @Module({
   controllers: [MusicController],
-  providers: [MusicService, JwtAuthService, MusicBookRepository, MelonService],
+  providers: [
+    MusicService,
+    JwtAuthService,
+    MusicBookRepository,
+    MusicBookLikeRepository,
+    MusicBookSourceRepository,
+    MelonService,
+    CloudflareImagesService,
+  ],
 })
 export class MusicModule {}
