da026828b564df4ccaea534110e956e6b4cb54f4
Kang Minsu <skymin0417@gmail.com>
2023-03-16 23:23:23 +0900

    fix: ecosystem.config.js 엔트리 경로 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'musicbook-be',
-      script: './dist/main.js',
+      script: './dist/src/main.js',
       instances: 2,
       exec_mode: 'cluster',
       time: true,
