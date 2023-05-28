b5b13ac52cf49940e12c1f8fb4ed5036681ef84e
Kang Minsu <skymin0417@gmail.com>
2023-05-28 23:33:32 +0900

    save

--- src/route/user/user.controller.ts
+++ src/route/user/user.controller.ts
@@ -54,11 +54,13 @@ export class UserController {
   logout(@Req() _req: Request, @Res({ passthrough: true }) _res: Response) {
     const redirectURL =
       _req.headers.referer || 'https://musicbook.kr/community';
-    _res.cookie('jwt', '', {
-      maxAge: 0,
-      httpOnly: true,
-      domain: process.env.ROOT_DOMAIN,
-    });
+    _res
+      .cookie('jwt', '', {
+        maxAge: 0,
+        httpOnly: true,
+        domain: process.env.ROOT_DOMAIN,
+      })
+      .redirect(redirectURL);
   }
 
   @Get('login/twitch')
